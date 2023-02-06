e6ccd4fceb7e231f21c1c67233e4a90a57952340
leodbrs <leodb.scap@gmail.com>
2023-02-06 21:08:30 +0000

    Moving alembic command to Dockerfile

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# ROOT_DIR=$(pwd)
-
-# cd "$ROOT_DIR/database"
-# rm -fr "$ROOT_DIR/database/alembic/versions/*"
-# alembic revision --autogenerate
-# alembic upgrade heads
-
-# cd "$ROOT_DIR"
 python /usr/src/app/InitDb.py
-python /usr/src/app/main.py
+uvicorn main:app --host 0.0.0.0 --port 8080
